deea9bdc9cee8e05f851a437cd1111f6bfb7a11a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-05-01 12:53:36 -0300

    :sparkler: create - fun - loginRoute

--- app/api.js
+++ app/api.js
@@ -6,8 +6,23 @@ import { INVALID_USER_ERROR } from "./config/errors.js";
 
 import jwt from "jsonwebtoken";
 
+
+async function loginRoute(request, response) {
+  const { user, password } = JSON.parse(await once(request, "data"))
+  if (user !== VALID.user || password !== VALID.password) {
+    response.writeHead(400)
+    return response.end(JSON.stringify(INVALID_USER_ERROR))
+  }
+
+  const token = jsonwebtoken.sign({ user, message: 'heyduude' }, TOKEN_KEY)
+
+  response.end(JSON.stringify({ token }))
+}
+
+
+
 async function handler(req, res) {
-  return ":D";
+  res.send({HI:" :D"})
 }
 
 const app = createServer(handler).listen(3000, () =>  console.log("listening to 3000"));
